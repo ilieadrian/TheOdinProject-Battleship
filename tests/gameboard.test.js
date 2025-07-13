cd75fae46a9a69962b8ca6305e271914412741d3
I A <ilie.adrian1986@gmail.com>
2025-07-13 20:32:06 +0300

    added test for place ships at specific coordinates by calling the ship factory or class

--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -3,10 +3,12 @@ import Ship from "../src/ship";
 
 let myGameboard = new Gameboard(10, 10);
 
+
 test('placeShip should place a ship on the gameboard', () => {
-    const myShip = new Ship(3);
-    myGameboard.placeShip(myShip, 0, 0, 'horizontal');
-    expect(myGameboard.generateBoard()[0][0]).toBe(myShip);
-    expect(myGameboard.generateBoard()[0][1]).toBe(myShip);
-    expect(myGameboard.generateBoard()[0][2]).toBe(myShip);
+  const myShip = new Ship(3);
+  myGameboard.placeShip(myShip, 0, 0, 'horizontal');
+
+  expect(myGameboard.gameboard[0][0]).toBe(myShip);
+  expect(myGameboard.gameboard[0][1]).toBe(myShip);
+  expect(myGameboard.gameboard[0][2]).toBe(myShip);
 });
