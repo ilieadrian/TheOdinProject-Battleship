d62066c2038cfc5af6cde7d35809c3da9f9966af
I A <ilie.adrian1986@gmail.com>
2025-08-21 16:46:28 +0300

    adapted the boards to player and CPU / added code to get the cell clicked on CPU board

--- src/index.js
+++ src/index.js
@@ -27,8 +27,8 @@ function newGame() {
   player2.gameboard.placeShip(new Ship(4), 5, 3, "vertical");
   player2.gameboard.placeShip(new Ship(4), 1, 8, "vertical");
 
-  const player1Grid = document.getElementById("player1-grid");
-  const player2Grid = document.getElementById("player2-grid");
+  const player1Grid = document.getElementById("player-grid");
+  const player2Grid = document.getElementById("cpu-grid");
 
   player1.gameboard.printBoard();
   player2.gameboard.printBoard();
@@ -40,8 +40,29 @@ function newGame() {
   // player1.gameboard.receiveAttack(0, 0)
   player2.gameboard.receiveAttack(3, 1)
 
+  const enemyBoardContainer = document.getElementById("cpu-grid");
+
+enemyBoardContainer.addEventListener("click", (e) => {
+  if (!e.target.dataset.x || !e.target.dataset.y) return;
+
+  const x = parseInt(e.target.dataset.x, 10);
+  const y = parseInt(e.target.dataset.y, 10);
+
+  try {
+    console.log(x, y);
+  } catch (err) {
+    console.error(err.message);
+    return;
+  }
+
+  // re-render board to show updated state
+  renderBoard(player2.gameboard.gameboard, enemyBoardContainer);
+});
+
   renderBoard(player1.gameboard.gameboard, player1Grid, true); // show player's ships
   renderBoard(player2.gameboard.gameboard, player2Grid, true); // hide computer ships
 }
+
+
 
 newGame();
